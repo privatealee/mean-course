300d42bbd3caf5500989fe56c0302db570415c83
Alejandra <privatealee>
2022-11-14 17:06:07 -0500

    working node server posting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostService } from './posts/post.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -31,7 +32,8 @@ import { PostService } from './posts/post.service';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 import { Subject } from "rxjs";
 
 import { Post } from "./post.model";
@@ -8,8 +9,17 @@ export class PostService {
   private posts: Post[] = [];
   private postUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts]; // how to copy an array
+    // sending http request to server
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      // listen
+      .subscribe((postData) => {
+        // no need to duplicate it since it's coming from the server
+        this.posts = postData.posts;
+        this.postUpdated.next([...this.posts]) // how to copy an array (so we can't edit incoming posts)
+      });
   }
 
   getPostUpdateListener(){
@@ -17,8 +27,13 @@ export class PostService {
   }
 
   addPost(title: string, content:string){
-    const post: Post = {title: title, content: content};
-    this.posts.push(post);
-    this.postUpdated.next([...this.posts]);
+    const post: Post = {id: null, title: title, content: content};
+
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData.message);
+        this.posts.push(post);
+        this.postUpdated.next([...this.posts]);
+      });
   }
 }
